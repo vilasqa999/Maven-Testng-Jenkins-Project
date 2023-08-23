2bdfcb437a639495dea523f8bfd6d2bcc69c7145
vilas giri <vilasgiri1001@gmail.com>
2023-08-23 16:41:16 +0530

    edge browser changed

--- src/test/java/mavenforjenkins/UITest.java
+++ src/test/java/mavenforjenkins/UITest.java
@@ -2,6 +2,7 @@ package mavenforjenkins;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -12,8 +13,12 @@ public class UITest
 	@Test	
 	public void startBrowser() throws InterruptedException
 	{
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver=new ChromeDriver();
+		//WebDriverManager.chromedriver().setup();
+		
+		WebDriverManager.edgedriver().setup();
+		//WebDriver driver=new ChromeDriver();
+		
+		WebDriver driver=new EdgeDriver();
 		driver.manage().window().maximize();
 		
 		driver.get("https://visualstories.com/info/whitelabel");
@@ -24,7 +29,7 @@ public class UITest
 		
 		System.out.println("Script executed successfully........");
 		
-		Thread.sleep(3000);
+		Thread.sleep(5000);
 		driver.quit();
 		
 	}
